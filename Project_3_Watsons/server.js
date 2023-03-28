485adf0ae619caafa5c3a4b4e1110c8a3aaac828
LeJared95 <122983113+LeJared95@users.noreply.github.com>
2023-03-28 17:53:00 +0800

    Update server.js

--- Project_3_Watsons/server.js
+++ Project_3_Watsons/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const logger = require("morgan");
+const mongoose = require("mongoose");
 require("dotenv").config();
 require("./config/database");
 
