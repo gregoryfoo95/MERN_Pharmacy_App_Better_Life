b6e983e36292bad895e07be96d3f3aa6d8423b59
LeJared95 <122983113+LeJared95@users.noreply.github.com>
2023-03-29 20:44:19 +0800

    Fix Issues

--- Project_3_Watsons/src/main.jsx
+++ Project_3_Watsons/src/main.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./pages/App/App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
