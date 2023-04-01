18123e587dcc965f1953895d8f255249a0c87462
faizalrahim <faizzzmart@gmail.com>
2023-04-01 11:53:22 +0800

    For Suffian

--- map/server.js
+++ map/server.js
@@ -1,20 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
 const mapRoutes = require('./routes/mapRoutes');
-
 
 dotenv.config();
 
 const app = express();
 
-mongoose.connect(process.env.DATABASE_URL,{})
-.then(() => console.log('Connected to MongoDB'))
-.catch((error) => console.error('Error connecting to MongoDB:', error));
+// Enable CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+mongoose.connect(process.env.DATABASE_URL, {})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((error) => console.error('Error connecting to MongoDB:', error));
 
 app.use(express.json());
-app.use('/api/map', mapRoutes);
+app.use('/api', mapRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
--- map/src/App.jsx
+++ map/src/App.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import './App.css';
 import MapComponent from './components/MapComponent';
 
-
 function App() {
   return (
-    <div id="map"></div>
+    <div className="App" style={{ width: '100vw', height: '100vh' }}>
+      <MapComponent zoom={17} />
+    </div>
   );
 }
 
