8658955870b8757a2a5814b47368231805c3f057
faizalrahim <faizzzmart@gmail.com>
2023-04-01 11:42:40 +0800

    V0.1

--- map/server.js
+++ map/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
 const mapRoutes = require('./routes/mapRoutes');
 
 
@@ -13,7 +14,7 @@ mongoose.connect(process.env.DATABASE_URL,{})
 .catch((error) => console.error('Error connecting to MongoDB:', error));
 
 app.use(express.json());
-app.use('/api', mapRoutes);
+app.use('/api/map', mapRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
--- map/src/App.jsx
+++ map/src/App.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import './App.css';
 import MapComponent from './components/MapComponent';
 
+
 function App() {
   return (
-    <div className="App" style={{ width: '100vw', height: '100vh' }}>
-      <MapComponent zoom={17} />
-    </div>
+    <div id="map"></div>
   );
 }
 
