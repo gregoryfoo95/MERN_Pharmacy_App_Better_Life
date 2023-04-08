37310a09acf94f102169120595e4d5cb8e3a0f93
GREGFOO\User <gregoryfoo@hotmail.com>
2023-04-09 02:00:34 +0800

    fix: Remove protect from all routes

--- routes/medicineRoute.js
+++ routes/medicineRoute.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const medicineController = require('../controllers/medicineController');
-const protect = require('../utils/middleware/authMiddleware')
 
 // Fetch and Input Data into DB from data.gov.sg
 router.get("/data", medicineController.data)
@@ -12,12 +11,12 @@ router.post('/', medicineController.create);
 router.get('/', medicineController.getAll);
 
 // Read a medicine by ID
-router.get('/:id',protect, medicineController.show);
+router.get('/:id', medicineController.show);
 
 // Update a medicine by ID
-router.put('/:id',protect, medicineController.updateById);
+router.put('/:id', medicineController.updateById);
 
 // Delete a medicine by ID
-router.delete('/:id',protect, medicineController.deleteById);
+router.delete('/:id', medicineController.deleteById);
 
 module.exports = router;
