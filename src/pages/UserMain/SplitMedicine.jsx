850b8910a1ce44d3baf812b39a017a02ab964b4b
faizalrahim <faizzzmart@gmail.com>
2023-04-10 00:53:14 +0800

    Update SplitMedicine.jsx

--- src/pages/UserMain/SplitMedicine.jsx
+++ src/pages/UserMain/SplitMedicine.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import MedicineImg from '../../../images/UserMainImg/imgMedicine.png';
+// import MedicineImg from '/images/UserMainImg/imgMedicine.png';
 
 export default function SplitMedicine() {
   return (
@@ -8,7 +8,7 @@ export default function SplitMedicine() {
       <div className="">
         <Link to="/map" style={{ textDecoration: 'none' }}>
           <img
-            src= {MedicineImg}
+            src= {'/images/UserMainImg/imgMedicine.png'}
             alt="Watson Pharmacy"
             style={{ width: '300px', height: '250px' }}
           />
