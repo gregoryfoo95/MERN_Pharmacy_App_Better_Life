b021c66344cd9a7c105ecedbd819bfb8f49a7b73
GREGFOO\User <gregoryfoo@hotmail.com>
2023-04-10 21:34:27 +0800

    trial fix: enable cors for deployment

--- server.js
+++ server.js
@@ -93,3 +93,19 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
+
+const allowedOrigins = ['http://localhost:3000',
+                      'https://pharmacy-app.onrender.com'];
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
