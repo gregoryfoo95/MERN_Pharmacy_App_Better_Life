639d115a8d7d68f5c9bd7d7e2ebccddaf7c15e7a
GREGFOO\User <gregoryfoo@hotmail.com>
2023-04-12 00:49:57 +0800

    Del stripeController and stripeRoute

--- routes/stripeRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const stripeController = require('../controllers/stripeController');
-
-router.post('/v1/products', stripeController.createProducts);
-
-module.exports = router;
