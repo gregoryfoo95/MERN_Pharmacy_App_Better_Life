4b499747fd0d1b52eb777e2b199990894a6e6128
GREGFOO\User <gregoryfoo@hotmail.com>
2023-04-11 11:42:02 +0800

    trial fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:3000",
+      "/api": "https://pharmacy-app-front.onrender.com",
     }
   },
   build: {
